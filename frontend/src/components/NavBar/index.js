8cc3f7b8134123c194795aa8eb331725a96c77da
hinddaoud <hhinddaoud@gmail.com>
2022-10-06 09:45:10 +0300

    log out logo

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -9,7 +9,7 @@ const Navbar=()=>{
 <div className="mNav">a</div>
 <div className="mNav">a</div>
 <div className="mNav">a</div>
-<div className="mNav">a</div>
+<div className="mNav log"><img src="./assets/images/l2.png" ></img><h6>logout</h6></div>
 
         </div>
     )
